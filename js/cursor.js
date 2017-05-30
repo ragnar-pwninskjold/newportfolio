f7500ac6a602354bb8e616806664a7f30462523f
Sean Kochel <seankochel@gmail.com>
2017-05-30 13:51:19 -0400

    updated hero copy

--- js/cursor.js
+++ js/cursor.js
@@ -1,29 +1,29 @@
 $(function(){
 		$(".desc1").typed({
-			strings: ["I am a full-stack JavaScript developer..."
+			strings: ["I am a full-stack JavaScript developer"
 			],
 			typeSpeed: 60,
 			preStringTyped: function() {
 				$(".desc1").css("text-align", "center");
 			}
 		});
-		$(".desc2").typed({
-			strings: [ 
-			"I'm interested in EdTech, FinTech, and BioTech..."
+		// $(".desc2").typed({
+		// 	strings: [ 
+		// 	"I'm interested in EdTech, FinTech, and BioTech..."
 			
-			],
-			typeSpeed: 60,
-			preStringTyped: function() {
-				$(".desc2").css("text-align", "center");
-			}
-		});
-		$(".desc3").typed({
-			strings: [
-			"I currently use React, Node, and MongoDB..."
-			],
-			typeSpeed: 60,
-			preStringTyped: function() {
-				$(".desc3").css("text-align", "center");
-			}
-		});
+		// 	],
+		// 	typeSpeed: 60,
+		// 	preStringTyped: function() {
+		// 		$(".desc2").css("text-align", "center");
+		// 	}
+		// });
+		// $(".desc3").typed({
+		// 	strings: [
+		// 	"I currently use React, Node, and MongoDB..."
+		// 	],
+		// 	typeSpeed: 60,
+		// 	preStringTyped: function() {
+		// 		$(".desc3").css("text-align", "center");
+		// 	}
+		// });
 });
